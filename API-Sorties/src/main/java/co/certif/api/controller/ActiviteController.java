d9b67535365caef01a769773910e41a39e3e00d0
ATournel <a.tournel@hotmail.fr>
2018-12-03 16:37:20 +0100

    Added "find activities where name like 'On a testé'" method and limited queries results on pages

--- API-Sorties/src/main/java/co/certif/api/controller/ActiviteController.java
+++ API-Sorties/src/main/java/co/certif/api/controller/ActiviteController.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 
 import org.hibernate.annotations.OrderBy;
-import org.skife.jdbi.v2.sqlobject.customizers.MaxRows;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -27,18 +29,20 @@ public class ActiviteController {
 	private ActiviteRepository actiRepo;
 	@Autowired
 	private TagRepository tagRepo;
-	/*
-	public ActiviteController(ActiviteRepository actiRepo) {
-		super();
-		this.actiRepo = actiRepo;
-	}*/
 	
+	/*
 	//Read all activities
 	@CrossOrigin
 	@GetMapping("/all")
 	@OrderBy(clause = "date_activite DESC")
 	public List<Activite> getActivites() {
 		return actiRepo.findAll(new Sort(Sort.Direction.DESC, "dateActivite"));
+	}
+	*/
+	@CrossOrigin
+	@GetMapping("/all")
+	public Page<Activite> getActivites(int page, int size, Sort sort) {
+		return actiRepo.findAll(new PageRequest(page, size, sort));
 	}
 	
 	//Read one activity using its id
@@ -48,9 +52,18 @@ public class ActiviteController {
 		return actiRepo.findOne(id);
 	}
 	
+	//Read all activities with "On a testé"
+	@CrossOrigin
+	@GetMapping("/nom={nomActivite}")
+	public Page<Activite> getActiviteTestees(int page, int size, Sort sort, @PathVariable(value="nomActivite") String nomActivite) {
+		return actiRepo.findAllByNomActiviteLike(new PageRequest(page, size, sort), nomActivite+"%");
+	}
+	
+	
 	//Read all activities with the specified tag
 	@CrossOrigin
 	@GetMapping("/tag={idTag}")
+	@OrderBy(clause = "date_activite DESC")
 	public List<Activite> getActivitesByTag(@PathVariable(value="idTag") Long idTag) {
 		return actiRepo.findByTag(tagRepo.findOne(idTag), new Sort(Sort.Direction.DESC, "dateActivite"));
 	}
--- API-Sorties/src/main/java/co/certif/api/repository/ActiviteRepository.java
+++ API-Sorties/src/main/java/co/certif/api/repository/ActiviteRepository.java
@@ -2,16 +2,24 @@ package co.certif.api.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import co.certif.api.model.Activite;
 import co.certif.api.model.Tag;
 
 @Repository
-public interface ActiviteRepository extends JpaRepository<Activite, Long> {
+public interface ActiviteRepository extends JpaRepository<Activite, Long>, PagingAndSortingRepository<Activite, Long>{
+	
+	Page<Activite> findAll(Pageable pageable);
+			
+	Page<Activite> findAllByNomActiviteLike(Pageable pageable, String nomActivite);
 	
 	List<Activite> findByTag(Tag tag, Sort sort);
+
 	
 }
